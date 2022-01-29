b8afe660c594c8e7afc8f5fcda07f93e269c4cd7
Saitoria <saronisaitoria@gmail.com>
2022-01-29 21:18:36 +0300

    Client script

---
+++ Client.gd
@@ -0,0 +1,11 @@
+extends SceneTree
+
+func _init():
+	var socket = PacketPeerUDP.new()
+	socket.set_dest_address("127.0.0.1",4242)
+	socket.put_packet("Your message here".to_ascii())
+	
+	print("Exiting application ")
+	self.quit()
+	
+
