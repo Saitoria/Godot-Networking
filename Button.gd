2e84c0a627bafe60be1ae7f5a46aa5fc6ed64297
Saitoria <saronisaitoria@gmail.com>
2022-01-29 21:18:06 +0300

    Button script

---
+++ Button.gd
@@ -0,0 +1,15 @@
+extends Button
+
+
+
+func _ready():
+	$HTTPRequest.connect("request_completed",self,"_on_HTTPResquest_request_completed")
+	
+
+
+func _on_Button_pressed():
+	$HTTPRequest.request("https://jsonplaceholder.typicode.com/todos/1",PoolStringArray([]),false)
+
+func _on_HTTPResquest_request_completed(result, response_code, headers, body):
+	if(response_code==200):
+		get_parent().get_node("Label").text = body.get_string_from_utf8()
